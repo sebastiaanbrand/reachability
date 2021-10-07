776e3be57ca9203fa5ade0eee07d7a1376c272df
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2021-10-07 11:48:50 +0200

    added force and promela to benchmark script

--- reachability/bench_all.sh
+++ reachability/bench_all.sh
@@ -6,13 +6,24 @@
 # output files
 beem_vn_stats="bench_data/beem_vanilla_stats_bdd.csv"
 beem_sl_stats="bench_data/beem_sloan_stats_bdd.csv"
+beem_f_stats="bench_data/beem_force_stats_bdd.csv"
 petri_vn_stats="bench_data/petrinets_vanilla_stats_bdd.csv"
 petri_sl_stats="bench_data/petrinets_sloan_stats_bdd.csv"
+petri_f_stats="bench_data/petrinets_force_stats_bdd.csv"
 promela_vn_stats="bench_data/promela_vanilla_stats_bdd.csv"
 promela_sl_stats="bench_data/promela_sloan_stats_bdd.csv"
+promela_f_stats="bench_data/promela_force_stats_bdd.csv"
 awari_stats="bench_data/awari_stats_bdd.csv"
+
 beem_vn_stats_ldd="bench_data/beem_vanilla_stats_ldd.csv"
 beem_sl_stats_ldd="bench_data/beem_sloan_stats_ldd.csv"
+beem_f_stats_ldd="bench_data/beem_force_stats_ldd.csv"
+petri_vn_stats_ldd="bench_data/petrinets_vanilla_stats_ldd.csv"
+petri_sl_stats_ldd="bench_data/petrinets_sloan_stats_ldd.csv"
+petri_f_stats_ldd="bench_data/petrinets_force_stats_ldd.csv"
+promela_vn_stats_ldd="bench_data/promela_vanilla_stats_ldd.csv"
+promela_sl_stats_ldd="bench_data/promela_sloan_stats_ldd.csv"
+promela_f_stats_ldd="bench_data/promela_force_stats_ldd.csv"
 
 mkdir -p bench_data
 
@@ -30,21 +41,25 @@ done
 
 # process arguments for running subset of all benchmarks
 for var in "$@"; do
-    if [[ $var == 'beem' ]]; then bench_beem_vn=true; bench_beem_sl=true; fi
-    if [[ $var == 'petri' ]]; then bench_ptri_vn=true; bench_ptri_sl=true; fi
-    if [[ $var == 'promela' ]]; then bench_prom_vn=true; bench_prom_sl=true; fi
+    if [[ $var == 'beem-all' ]]; then bench_beem_vn=true; bench_beem_sl=true; bench_beem_f=true; fi
+    if [[ $var == 'petri-all' ]]; then bench_ptri_vn=true; bench_ptri_sl=true; bench_ptri_f=true; fi
+    if [[ $var == 'promela-all' ]]; then bench_prom_vn=true; bench_prom_sl=true; bench_prom_f=true; fi
     if [[ $var == 'beem-vanilla' ]]; then bench_beem_vn=true; fi
+    if [[ $var == 'beem-force' ]]; then bench_beem_f=true; fi
     if [[ $var == 'beem-sloan' ]]; then bench_beem_sl=true; fi
     if [[ $var == 'petri-vanilla' ]]; then bench_ptri_vn=true; fi
     if [[ $var == 'petri-sloan' ]]; then bench_ptri_sl=true; fi
+    if [[ $var == 'petri-force' ]]; then bench_ptri_f=true; fi
     if [[ $var == 'promela-vanilla' ]]; then bench_prom_vn=true; fi
     if [[ $var == 'promela-sloan' ]]; then bench_prom_sl=true; fi
+    if [[ $var == 'promela-force' ]]; then bench_prom_f=true; fi
     if [[ $var == 'awari' ]]; then bench_awari=true; fi
     if [[ $var == 'bdd' ]]; then bench_bdd=true; fi
     if [[ $var == 'ldd' ]]; then bench_ldd=true; fi
     if [[ $var == 'all' ]]; then
         bench_beem_vn=true; bench_ptri_vn=true; bench_prom_vn=true
         bench_beem_sl=true; bench_ptri_sl=true; bench_prom_sl=true
+        bench_beem_f=true;  bench_ptri_f=true;  bench_prom_f=true
         bench_bdd=true; bench_ldd=true
         bench_awari=true;
     fi
@@ -55,18 +70,24 @@ echo "Running following benchmarks:"
 
 if [[ $bench_beem_vn && $bench_bdd ]]; then echo "  - BEEM BDDs vanilla"; fi
 if [[ $bench_beem_sl && $bench_bdd ]]; then echo "  - BEEM BDDs Sloan"; fi
+if [[ $bench_beem_f  && $bench_bdd ]]; then echo "  - BEEM BDDs FORCE"; fi
 if [[ $bench_beem_vn && $bench_ldd ]]; then echo "  - BEEM LDDs vanilla"; fi
 if [[ $bench_beem_sl && $bench_ldd ]]; then echo "  - BEEM LDDs Sloan"; fi
+if [[ $bench_beem_f  && $bench_ldd ]]; then echo "  - BEEM LDDs FORCE"; fi
 
 if [[ $bench_ptri_vn && $bench_bdd ]]; then echo "  - Petri Nets BDDs vanilla"; fi
 if [[ $bench_ptri_sl && $bench_bdd ]]; then echo "  - Petri Nets BDDs Sloan"; fi
+if [[ $bench_ptri_f  && $bench_bdd ]]; then echo "  - Petri Nets BDDs FORCE"; fi
 if [[ $bench_ptri_vn && $bench_ldd ]]; then echo "  - Petri Nets LDDs vanilla"; fi
 if [[ $bench_ptri_sl && $bench_ldd ]]; then echo "  - Petri Nets LDDs Sloan"; fi
+if [[ $bench_ptri_f  && $bench_ldd ]]; then echo "  - Petri Nets LDDs Force"; fi
 
 if [[ $bench_prom_vn && $bench_bdd ]]; then echo "  - Petri Nets BDDs vanilla"; fi
 if [[ $bench_prom_sl && $bench_bdd ]]; then echo "  - Petri Nets BDDs Sloan"; fi
+if [[ $bench_prom_f  && $bench_bdd ]]; then echo "  - Petri Nets BDDs FORCE"; fi
 if [[ $bench_prom_vn && $bench_ldd ]]; then echo "  - Petri Nets LDDs vanilla"; fi
 if [[ $bench_prom_sl && $bench_ldd ]]; then echo "  - Petri Nets LDDs Sloan"; fi
+if [[ $bench_prom_f  && $bench_ldd ]]; then echo "  - Petri Nets LDDs FORCE"; fi
 
 if [[ $bench_awari && $bench_bdd ]]; then echo "  - Awari BDDs"; fi
 
@@ -95,6 +116,17 @@ if [[ $bench_beem_sl && $bench_bdd ]]; then
     done
 fi
 
+# BEEM, Force BDDs
+if [[ $bench_beem_f && $bench_bdd ]]; then
+    for filename in models/beem/bdds/force/*.bdd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$beem_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=sat --count-nodes --statsfile=$beem_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$beem_f_stats
+        done
+    done
+fi
+
 # Petri nets, vanilla BDDs
 if [[ $bench_ptri_vn && $bench_bdd ]]; then
     for filename in models/petrinets/bdds/vanilla/*.bdd; do
@@ -117,6 +149,17 @@ if [[ $bench_ptri_sl && $bench_bdd ]]; then
     done
 fi
 
+# Petri nets, Force BDDs
+if [[ $bench_ptri_f && $bench_bdd ]]; then
+    for filename in models/petrinets/bdds/force/*.bdd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$petri_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=sat --count-nodes --statsfile=$petri_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$petri_f_stats
+        done
+    done
+fi
+
 # Promela, vanilla BDDs
 if [[ $bench_prom_vn && $bench_bdd ]]; then
     for filename in models/promela/bdds/vanilla/*.bdd; do
@@ -135,6 +178,17 @@ if [[ $bench_prom_sl && $bench_bdd ]]; then
             timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$promela_sl_stats
             timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=sat --count-nodes --statsfile=$promela_sl_stats
             timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$promela_sl_stats
+        done
+    done
+fi
+
+# Promela, Force BDDs
+if [[ $bench_prom_f && $bench_bdd ]]; then
+    for filename in models/promela/bdds/force/*.bdd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$promela_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=sat --count-nodes --statsfile=$promela_f_stats
+            timeout $maxtime ./../build/reachability/bddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$promela_f_stats
         done
     done
 fi
@@ -171,6 +225,17 @@ if [[ $bench_beem_sl && $bench_ldd ]]; then
     done
 fi
 
+# BEEM, Force LDDs (merging relation for BFS and REC requires overapproximated LDDs)
+if [[ $bench_beem_f && $bench_ldd ]]; then
+    for filename in models/beem/ldds/force/overapprox/*.ldd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$beem_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc models/beem/ldds/force/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$beem_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$beem_f_stats_ldd
+        done
+    done
+fi
+
 # Petri nets, vanilla LDDs (merging relation for BFS and REC requires overapproximated LDDs)
 if [[ $bench_ptri_vn && $bench_ldd ]]; then
     for filename in models/petrinets/ldds/vanilla/overapprox/*.ldd; do
@@ -183,12 +248,56 @@ if [[ $bench_ptri_vn && $bench_ldd ]]; then
 fi
 
 # Petri nets, Sloan LDDs (merging relation for BFS and REC requires overapproximated LDDs)
-if [[ $bench_ptri_vn && $bench_ldd ]]; then
+if [[ $bench_ptri_sl && $bench_ldd ]]; then
     for filename in models/petrinets/ldds/sloan/overapprox/*.ldd; do
         for nw in $num_workers; do
             timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$petri_sl_stats_ldd
             timeout $maxtime ./../build/reachability/lddmc models/petrinets/ldds/sloan/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$petri_sl_stats_ldd
             timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$petri_sl_stats_ldd
+        done
+    done
+fi
+
+# Petri nets, Force LDDs (merging relation for BFS and REC requires overapproximated LDDs)
+if [[ $bench_ptri_f && $bench_ldd ]]; then
+    for filename in models/petrinets/ldds/force/overapprox/*.ldd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$petri_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc models/petrinets/ldds/force/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$petri_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$petri_f_stats_ldd
+        done
+    done
+fi
+
+# Promela, vanilla LDDs (merging relation for BFS and REC requires overapproximated LDDs)
+if [[ $bench_prom_vn && $bench_ldd ]]; then
+    for filename in models/promela/ldds/vanilla/overapprox/*.ldd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$promela_vn_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc models/promela/ldds/vanilla/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$promela_vn_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$promela_vn_stats_ldd
+        done
+    done
+fi
+
+# Promela, Sloan LDDs (merging relation for BFS and REC requires overapproximated LDDs)
+if [[ $bench_prom_sl && $bench_ldd ]]; then
+    for filename in models/promela/ldds/sloan/overapprox/*.ldd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$promela_sl_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc models/promela/ldds/sloan/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$promela_sl_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$promela_sl_stats_ldd
+        done
+    done
+fi
+
+# Promela, Force LDDs (merging relation for BFS and REC requires overapproximated LDDs)
+if [[ $bench_prom_f && $bench_ldd ]]; then
+    for filename in models/promela/ldds/force/overapprox/*.ldd; do
+        for nw in $num_workers; do
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=bfs --merge-relations --count-nodes --statsfile=$promela_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc models/promela/ldds/force/$(basename $filename) --workers=$nw --strategy=sat --count-nodes --statsfile=$promela_f_stats_ldd
+            timeout $maxtime ./../build/reachability/lddmc $filename --workers=$nw --strategy=rec --merge-relations --count-nodes --statsfile=$promela_f_stats_ldd
         done
     done
 fi
