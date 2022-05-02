14790a19c45872011398e138ac8e3177a85a5895
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2022-05-02 21:24:12 +0200

    bench total wall time for its-reach

--- scripts/bench_itstools.sh
+++ scripts/bench_itstools.sh
@@ -10,7 +10,9 @@ maxtime=10m
 itstools=scripts/bin/its-reach-linux64
 
 outputfolder_petri=bench_data/its_tools/petrinets
-mkdir -p $outputfolder_petri
+
+dt=$(date '+%Y%m%d_%H%M%S');
+outputfolder_petri=$outputfolder_petri/$dt
 
 # process arguments for running subset of all benchmarks
 for var in "$@"; do
@@ -27,13 +29,14 @@ if [[ $bench_petri ]]; then
     echo "- benching petri; writing results to $outputfolder_petri"
 fi
 read -p "Press enter to start"
+mkdir -p $outputfolder_petri
 
 # Petri nets, Sloan
 if [[ $bench_petri ]]; then
     for filename in models/petrinets/gal/*.gal; do
         out_file=$outputfolder_petri/$(basename $filename).out
         echo "benchmarking $filename"
-        timeout $maxtime $itstools -i $filename -t CGAL &> $out_file
+        (time timeout $maxtime $itstools -i $filename -t CGAL) |& tee $out_file
     done
 fi
 
