eb1dbea140ada38194756d1358bc6150be618cc7
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2022-11-18 14:20:19 +0100

    cleanup

--- scripts/temp_bench_multicore.sh
+++ scripts/temp_bench_multicore.sh
@@ -1,3 +1,3 @@
 
 ./scripts/bench_all.sh -o for_paper/par_64 -w "1 64" -t 10m test-par-only bdd beem-sloan petri-sloan promela-sloan no-confirm
-./scripts/bench_all.sh -o for_paper/par_16 -w 16 -t10m test-par-only bdd beem-sloan petri-sloan promela-sloan no-confirm
+./scripts/bench_all.sh -o for_paper/par_16 -w 16 -t 10m test-par-only bdd beem-sloan petri-sloan promela-sloan no-confirm
