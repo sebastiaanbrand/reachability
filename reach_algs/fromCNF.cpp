b1242628b82d07374f9d094dbc43608cb21a859c
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2023-01-06 14:53:11 +0100

    bfs for cnf inputs seems to work now

--- reach_algs/fromCNF.cpp
+++ reach_algs/fromCNF.cpp
@@ -34,6 +34,48 @@ BDD S, R, T; // initial states, relation, (optional) target states
 BDDSET vars; // state vars (i.e. vars of S) (even vars starting at 0)
 
 
+void print_lit(int lit)
+{
+    if (lit > 0) {
+        std::cout << "x" << lit;
+    } else if (lit < 0) {
+        std::cout << "~x" << std::abs(lit);
+    } else {
+        std::cerr << "literal 0 shouldn't occur in CNF formula" << std::endl;
+    }
+}
+
+void print_clause(Clause clause)
+{
+    int lit_prev = INT32_MAX;
+    std::cout << "(";
+    for (int lit : clause) {
+        if (lit_prev != INT32_MAX) {
+            print_lit(lit_prev);
+            std::cout << " v ";
+        }
+        lit_prev = lit;
+    }
+    // print last lit
+    print_lit(lit_prev);
+    std::cout << ")";
+}
+
+void print_cnf(Cnf cnf)
+{
+    Clause clause_prev;
+    for (Clause clause : cnf) {
+        if (clause_prev.size() != 0) {
+            print_clause(clause_prev);
+            std::cout << " ^ ";
+        }
+        clause_prev = clause;
+    }
+    print_clause(clause_prev);
+    std::cout << std::endl;
+}
+
+
 /**
  * Loads a CNF fromula from a DIMACS (.cnf) file.
  */
@@ -52,8 +94,10 @@ Cnf cnf_from_file(std::string filepath)
                 if (token == "p" || token == "cnf") {
                     break;
                 }
-                int var = std::stoi(token);
-                clause.insert(var);
+                int lit = std::stoi(token);
+                if (lit != 0) {
+                    clause.insert(lit);
+                }
             }
             if (clause.size() > 0) {
                 res.insert(clause);
@@ -122,23 +166,38 @@ Bdd cnf_to_bdd(Cnf f)
 
 void parse_args(int argc, char** argv)
 {
-    if (argc < 2) {
+    if (argc < 4) {
         Abort("Usage: ./fromCNF initial.cnf relation.cnf [target.cnf]\n");
     }
 
     // Get S, R, and T
-    S = cnf_to_bdd(cnf_from_file(argv[0])).GetBDD();
-    R = cnf_to_bdd(cnf_from_file(argv[1])).GetBDD();
-    T = sylvan_false;
-    if (argc >= 3) {
-        T = cnf_to_bdd(cnf_from_file(argv[2])).GetBDD();
-    }
+    Cnf cnf_s = cnf_from_file(argv[1]);
+    Cnf cnf_targ = cnf_from_file(argv[3]);
+    Cnf cnf_r = cnf_from_file(argv[2]);
+
+    // convert CNFs to BDDs
+    S = cnf_to_bdd(cnf_s).GetBDD();
+    R = cnf_to_bdd(cnf_r).GetBDD();
+    T = cnf_to_bdd(cnf_targ).GetBDD();
+
+    printf("Source:\n");
+    print_cnf(cnf_s);
+    printf("Target:\n");
+    print_cnf(cnf_targ);
 
     // Get state vars (assumes state vars = [0, 2, 4, ...,2*(n-1)])
-    int nvars = nvars_from_file(argv[0]);
+    int nvars = nvars_from_file(argv[1]);
+    if (nvars == 0) {
+        Abort("Something went wrong parsing nvars from %s\n", argv[1]);
+    }
     uint32_t vars_arr[nvars];
-    for (int i = 0; i < nvars; i++) vars_arr[i] = 2*i;
+    for (int i = 0; i < nvars; i++) {
+        vars_arr[i] = 2*i;
+    }
     vars = sylvan_set_fromarray(vars_arr, nvars);
+
+    INFO("Source set has %'0.0f state(s)\n", sylvan_satcount(S, vars));
+    INFO("Target set has %'0.0f state(s)\n", sylvan_satcount(T, vars));
 }
 
 
@@ -157,11 +216,28 @@ int main(int argc, char** argv)
     sylvan_init_bdd();
     
     parse_args(argc, argv);
-    BDD reachable = bdd_reach(R, S, vars);
+    
+
+    // simple BFS
+    // TODO: refactor BFS in bddmc to bdd_reach_algs, use that one
+    // TODO: bdd_reach for this // BDD reachable = bdd_reach(R, S, vars);
+    //       FIX: add a's to state and a-primes to rel
+    BDD reachable = S;
+    BDD prev = sylvan_false;
+    while (prev != reachable) {
+        prev = reachable;
+        BDD successors = sylvan_relnext(reachable,  R, vars);
+        reachable = sylvan_or(reachable, successors);
+        INFO("Reachable states: %'0.0f state(s)\n", sylvan_satcount(reachable, vars));
+    }
+
+    FILE *fp = fopen("reachable.dot", "w");
+    sylvan_fprintdot(fp, reachable);
+    fclose(fp);
 
     
     double reach_states = sylvan_satcount(reachable, vars);
-    INFO("Final states: %'0.0f states\n", reach_states);
+    INFO("Reachable states: %'0.0f state(s)\n", reach_states);
 
     if (T != sylvan_false) {
         BDD intersection = sylvan_and(reachable, T);
